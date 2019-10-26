d6b2aebd4620753a3c6178157d2f28630116cb41
Vijay <patelv60@mcmaster.ca>
2019-10-26 19:49:27 -0400

    Update logins and initial JSON

--- initialJSON.py
+++ initialJSON.py
@@ -1,6 +1,7 @@
 #Script used to create and initialize the users JSON file
 import json
 
+#initialize count to 0 and recent to blank
 users = {
     "count":0,
     "recent":"",
--- logins.py
+++ logins.py
@@ -1,6 +1,7 @@
 import json
 from tkinter import *
 
+# Checks JSON object for User count to ensure that maximum of 10 users is maintained
 def checkCountUsers():
     with open('users.json') as json_file:
         data = json.load(json_file)
@@ -9,21 +10,24 @@ def checkCountUsers():
         else:
             return True
 
+#Registers NeW uSeRs into the JSON file
 def register(username, password):
+    #Checks user count to ensure that more than 10 users are no created
+    #Checks username to ensure that it doesn't already exist
     if checkCountUsers() == True and checkUsername(username)== False:
         with open('users.json') as json_file:
             data = json.load(json_file)
-
             count = data["count"]
             count +=1
             data["count"] = count
+            #initialize user information
             data.update({username:{
             "password":password,
             "aoo":{"lower":"","upper":"","AAmp":"", "APW":""},
             "voo":{"lower":"","upper":"","VAmp":"", "VPW":""},
             "aai":{"lower":"","upper":"","AAmp":"", "APW":"", "ARP":""},
              "vvi":{"lower":"","upper":"","VAmp":"", "VPW":"", "VRP":""}}})
-
+        #dumps info into JSON file
         with open('users.json', 'w') as json_file:
             json.dump(data, json_file)
 
@@ -31,6 +35,7 @@ def register(username, password):
     else:
         return False
 
+#Checks if username already exists (for register function)
 def checkUsername(username):
     with open('users.json') as json_file:
         data = json.load(json_file)
@@ -40,6 +45,7 @@ def checkUsername(username):
     else:
         return False
 
+#Validates if correct password was inputed
 def checkPassword(username, password):
     if checkUsername(username):
         with open('users.json') as json_file:
@@ -51,6 +57,7 @@ def checkPassword(username, password):
     else:
         return False
 
+#Stores the most recent user login
 def recent(username):
     with open('users.json') as json_file:
         data = json.load(json_file)
@@ -59,6 +66,7 @@ def recent(username):
     with open('users.json', 'w') as json_file:
         json.dump(data, json_file)
 
+#Updates parameters for each mode (called in main.py to update values)
 def update(username, mode, key, value):
     with open('users.json') as json_file:
         data = json.load(json_file)
@@ -68,6 +76,7 @@ def update(username, mode, key, value):
     with open('users.json', 'w') as json_file:
         json.dump(data, json_file)
 
+#deletes user from JSON and updates count
 def deleteUser(username):
     with open('users.json') as json_file:
         data = json.load(json_file)
@@ -79,6 +88,7 @@ def deleteUser(username):
     with open('users.json', 'w') as json_file:
         json.dump(data, json_file)
 
+#general purpose alert window to display confirmations and errors
 def alert(message):
     window = Toplevel()
     #window.minsize(300, 300)
